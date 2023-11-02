6a06bf81c4a84eb2d82f264e82f0f86f5128812f
Bramsurya <bram.j@emveep.com>
2023-11-02 12:56:29 +0700

    fix: fix some header link styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
 								className={clsx(
 									'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300 transition',
 									{
-										'text-gray-950 dark:text-gray-300':
+										'text-gray-950 dark:!text-gray-300':
 											activeSection === link.name,
 									},
 								)}
