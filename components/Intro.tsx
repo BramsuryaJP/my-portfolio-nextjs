fbe571918faaaa25bf219756560521764aca7818
Bramsurya <bram.j@emveep.com>
2023-11-29 08:55:35 +0700

    feat: change linked in profile url

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -98,7 +98,7 @@ export default function Intro() {
 				<a
 					className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full outline-none
           focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-					href='https://www.linkedin.com/in/bramsurya-johannes-paulus-a6bb03241/'
+					href='https://www.linkedin.com/in/bramsurya-johannes-paulus/'
 					target='_blank'
 				>
 					<BsLinkedin />
