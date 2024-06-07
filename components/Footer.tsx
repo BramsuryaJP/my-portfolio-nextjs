de640133cf715ba0ec0d0da5e028ae29221acf69
bramsurya <bramsuryajohannespaulus.work@gmail.com>
2024-06-08 06:34:22 +0700

    feat: add internationalization for footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,19 @@
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 export default function Footer() {
+	const t = useTranslations('Footer')
+
 	return (
 		<footer className='mb-10 px-4 text-center text-gray-500 dark:text-white/70'>
 			<small className='mb-2 block text-xs'>
 				&copy; 2023 Bramsurya. All rights reserved.
 			</small>
 			<p className='text-xs'>
-				<span className='font-semibold'>About this website:</span> built with
-				React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-				Framer Motion, React Email & Resend, Vercel hosting.
+				<span className='font-semibold'>{t('footerAbout')}:</span>{' '}
+				{t('footerMade')} React & Next.js (App Router & Server Actions),
+				TypeScript, Tailwind CSS, Framer Motion, Next Internationalization
+				(i18n), React Email & Resend, Vercel hosting.
 			</p>
 		</footer>
 	)
