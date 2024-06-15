b83b6a96bdea72791cef3ef95710db94f66f5d06
bramsurya <bramsuryajohannespaulus.work@gmail.com>
2024-06-15 23:31:14 +0700

    style: add z index for lang and theme toggler

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -9,7 +9,7 @@ export default function ThemeToggler() {
 
 	return (
 		<button
-			className='fixed bottom-8 right-5  bg-white w-[3rem] h-[3rem] bg-opacity-80 
+			className='fixed bottom-8 right-5  bg-white w-[3rem] h-[3rem] bg-opacity-80 z-10
     backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex 
     items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'
 			onClick={toggleTheme}
