c81c2a1106774f431be49550dd94c2197ce5d119
bramsurya <bramsuryajohannespaulus.work@gmail.com>
2024-06-17 11:58:55 +0700

    feat: add 404 not found page

--- app/[locale]/blog/[slug]/not-found.tsx
+++ app/[locale]/blog/[slug]/not-found.tsx
@@ -1,9 +1,34 @@
 'use client'
 
+import { Link, useRouter } from '@/navigation'
+
 export default function NotFound() {
+	const router = useRouter()
+
 	return (
-		<div>
-			<h2>404 not found</h2>
+		<div className='flex items-center justify-center h-screen'>
+			<section className='h-dvh'>
+				<div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
+					<div className='mx-auto max-w-screen-sm text-center'>
+						<h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500'>
+							404
+						</h1>
+						<p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+							Something's missing.
+						</p>
+						<p className='mb-4 text-lg font-light text-gray-900 dark:text-white'>
+							Sorry we couldn't find the blog you were looking for
+						</p>
+						<Link
+							type='button'
+							href={'/blog'}
+							className='bg-gray-100 dark:bg-white/10 border border-black/5 rounded-md inline-flex text-black dark:text-white bg-primary-600 font-medium text-sm px-5 py-2.5 text-center my-4'
+						>
+							Back to Blog Page
+						</Link>
+					</div>
+				</div>
+			</section>
 		</div>
 	)
 }
